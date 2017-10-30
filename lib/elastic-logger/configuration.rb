f01abe3e25953055a5f9d806b6eaa4ee11a64352
Radosław Woźniak <rad.wozniak@gmail.com>
2017-10-30 14:58:41 +0100

    moves severity to log instance level

--- lib/elastic-logger/configuration.rb
+++ lib/elastic-logger/configuration.rb
@@ -1,13 +1,12 @@
 module ElasticLogger
   class Configuration
-    attr_accessor :host, :types_file, :path, :prefix, :log_level
+    attr_accessor :host, :types_file, :path, :prefix
 
     def initialize
       @host = 'localhost:9200'
       @types_file = 'config/elastic_log_types.yml'
       @path = 'log'
       @prefix = ''
-      @log_level = ::Logger::DEBUG
     end
   end
 end
--- lib/elastic-logger/logger.rb
+++ lib/elastic-logger/logger.rb
@@ -3,8 +3,9 @@ require 'elastic-logger/types'
 module ElasticLogger
   class Logger
 
-    def initialize(name)
+    def initialize(name, log_level = 'debug')
       @name = name.to_s
+      @log_level = log_level
     end
 
     # def debug(hash)
@@ -15,31 +16,27 @@ module ElasticLogger
     # def warn(hash)
     %w(debug error fatal info unknown warn).each do |severity|
       define_method(severity) do |hash|
-        log(hash, severity.to_s.upcase)
+        log(hash, severity)
       end
     end
 
-    def log(msg, severity = 'UNKNOWN')
-      return true if ::Logger.const_get(severity) < config.log_level
+    def log(msg, severity = 'unknown')
+      return true if skip_logging?(severity)
 
-      writer.log(severity, format(msg))
+      writer.log(format_severity(severity), format_message(msg))
       true
     end
 
     private
-    attr_reader :name
+    attr_reader :name, :log_level
+
+    def skip_logging?(severity)
+      log_levels[severity] < log_levels[log_level]
+    end
 
     def writer
       writer = logs.fetch(name, default).fetch("writer")
       Object.const_get(writer).new(name: log_name, config: config)
-    end
-
-    def format(msg)
-      case msg
-      when String then { msg: msg }
-      when Array then msg.map.with_index { |v, i| [i, v] }.to_h
-      else msg
-      end
     end
 
     def log_name
@@ -56,6 +53,24 @@ module ElasticLogger
 
     def default
       { "writer" => "ElasticLogger::NullLogger" }
+    end
+
+    def log_levels
+      @log_levels ||= Hash.new do |hash, key|
+        hash[key] = ::Logger.const_get(format_severity(severity))
+      end
+    end
+
+    def format_severity(severity)
+      severity.to_s.upcase
+    end
+
+    def format_message(msg)
+      case msg
+      when String then { msg: msg }
+      when Array then msg.map.with_index { |v, i| [i, v] }.to_h
+      else msg
+      end
     end
   end
 end
